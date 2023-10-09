0fe3b659755435b6308721317461bf366087fd7d
Fabian Ortega <fabian.ortega501@gmail.com>
2023-10-09 00:57:32 -0500

    Ejemplo_CRUD_VUE

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Gestión Académica</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>          
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Estudiantes
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <router-link class="dropdown-item" to="/">Listar Estudiantes</router-link>                
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/createE">Registrar nuevo</router-link>                
+              </li>              
+              <li>
+                <hr class="dropdown-divider">
+              </li>              
+            </ul>
+          </li>          
+        </ul>        
+      </div>
+    </div>
+    </nav>
+    <div class="container-fluid">      
+      <router-view />
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="row">
+    <div class="col-lg-8 offset-lg-2">
+      <div class="table-responsive">
+        <table class="table table-border table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>ID</th>
+              <th>FOTO</th>
+              <th>NOMBRE</th>
+              <th>APELLIDO</th>
+              <th>REGISTRO</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider" id="contenido">
+            <tr v-if="this.cargando">
+              <td colspan="6"><h3>Cargando</h3></td>              
+            </tr>
+            <tr v-else v-for="est, i  in this.estudiantes" :key="est.id">
+              <td v-text="(i+1)"></td>
+              <td v-text="est.id"></td>
+              <td>
+                <img v-if="est.foto" style="width: 150px !important;" :src="est.foto" class="img-thumbnail">
+                <img v-else style="width: 150px !important;" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-256.png" class="img-thumbnail" alt="">
+              </td>
+              <td v-text="est.nombre"></td>
+              <td v-text="est.apellido"></td>
+              <td v-text="new Date(est.created_at).toLocaleDateString('en-US')"></td>
+              <td>
+                <router-link :to="{path:'viewE/'+est.id}" class="btn btn-info">
+                  <i class="fa-solid fa-eye"></i>
+                </router-link>
+                &nbsp;
+                <router-link :to="{path:'editE/'+est.id}" class="btn btn-warning">
+                  <i class="fa-solid fa-edit"></i>
+                </router-link>
+                &nbsp;
+                <button class="btn btn-danger" v-on:click="$event => eliminar(est.id, est.nombre)">
+                  <i class="fa-solid fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  import {confirmar} from '../funciones';
+
+  export default{
+    data(){
+      return{
+        estudiantes:null,
+        cargando:false
+      }
+    },
+    mounted(){
+      this.getEstudiantes();
+    },
+    methods:{
+      getEstudiantes(){
+        this.cargando = true;
+        axios.get('http://localhost/cursos/ejemploCrud/public/api/v1/estudiantes').then(
+          res => {
+            this.estudiantes = res.data;
+            this.cargando = false;
+          }
+        );
+      },
+      eliminar(id,nombre){
+        confirmar('http://localhost/cursos/ejemploCrud/public/api/v1/estudiantes/',id,'Eliminar Registro','Realmente desea eliminar a '+nombre+'?');
+        this.cargando = false;
+      }
+    }
+  }
+</script>
